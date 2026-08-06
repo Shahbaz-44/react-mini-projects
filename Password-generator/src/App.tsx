72dfcae59036e525cca50b64f6446cc6b158e43c
Shahbaz Haider <alibhaialiali774@gmail.com>
2026-08-06 20:18:02 +0500

    Add password length and character options

--- Password-generator/src/App.tsx
+++ Password-generator/src/App.tsx
@@ -1,8 +1,19 @@
-import { useState } from 'react'
+import { useState , useCallback} from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [length, setLength] = useState(8)
+  const [numberAllow, setNumberAllow] = useState (false)
+  const [charAllow, setCharAllow] = useState (false)
+
+  const [password, setPassword] = useState ("")
+
+  const passwordGenerator =  useCallback(() => {
+    let pass = ""
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+    if (numberAllow) str += "0123456789"
+    
+  }, [length, numberAllow, charAllow, setPassword])
 
   return (
     <>
