52b3ae80e4bd5d777e670ee3f5a5809b727383c9
Shahbaz Haider <alibhaialiali774@gmail.com>
2026-08-21 18:28:46 +0500

    Implement useId for unique element IDs

--- currencyConvertor/src/App.tsx
+++ currencyConvertor/src/App.tsx
@@ -1,9 +1,14 @@
+import InputBox from "./components/InputBox"
+
+
+
 function App() {
 
 
   return (
     <>
     <h1 className='text-3xl bg-blue-300 bold text-center p-10'>Currency Convertor</h1>
+    <InputBox />
     </>
   )
 }
--- currencyConvertor/src/components/InputBox.tsx
+++ currencyConvertor/src/components/InputBox.tsx
@@ -1,4 +1,4 @@
-
+import { useId } from "react";
 // function InputBox({
 //     label,
     
@@ -48,6 +48,8 @@
 
 
 
+
+
 interface InputBoxProps {
   label: string;
   amount: number;
@@ -72,15 +74,19 @@ function InputBox({
   currencyDisable = false,
   className = "",
 }: InputBoxProps) {
+  const amountInputId = useId()      
+
   return (
     <div className={`bg-white p-3 rounded-lg text-sm flex ${className}`}>
       
       <div className="w-1/2">
-        <label className="text-black/40 mb-2 inline-block">
+        <label htmlFor={amountInputId}
+         className="text-black/40 mb-2 inline-block">
           {label}
         </label>
 
         <input
+          id={amountInputId}
           className="outline-none w-full bg-transparent py-1.5"
           type="number"
           placeholder="Amount"
@@ -100,6 +106,7 @@ function InputBox({
           className="rounded-lg px-1 py-1 bg-gray-100 cursor-pointer outline-none"
           value={selectCurrency}
           onChange={(e) => onCurrencyChange(e.target.value)}
+          disabled={currencyDisable}
         >
           {currencyOptions.map((currency) => (
             <option key={selectCurrency} value={currency}>
