a6340a5809a9dfd7a0cc0b86da78edeb11aaae9a
Shahbaz Haider <alibhaialiali774@gmail.com>
2026-09-02 15:07:44 +0500

    explore react router

--- reactRouter/src/App.tsx
+++ reactRouter/src/App.tsx
@@ -1,14 +1,20 @@
 import { useState } from 'react'
 import './App.css'
-import Header  from './components/header/header'
+// import Header  from './components/header/header'
+// import Footer from './components/footer/footer'
+// import Home from './components/home/home'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    <Header/>
-    <h1 className='bg-green-300 p-4'>react router</h1>
+
+   {/* direct render ho gi main.tsx main RouterProvider sy */}
+
+    {/* <Header/>
+    <Home/>  
+    <Footer/> */}
     </>
   )
 }
